668d84c38e1e335a45ac30d78f4bbfe75c5d0bec
Jesse Rosenberger <git@jro.cc>
2020-03-30 15:02:44 +0300

    WIP: refactor: Introduce "cache-control" plugin and switch req. pipeline to it.
    > This commit message is WIP.  The commit is "done".
    
    Simialr to f3efe04b5c3, which migrated the tracing package to the plugin
    API, this commit introduces a `plugin` (named) export from the
    `apollo-cache-control` module.  It similarly accomplishes that by
    duplicating the behavior of the `CacheControlExtension` which leveraged the
    soon-to-be-deprecated `graphql-extensions`.  The functionality, again, is
    intended to be identical in spirit.
    
    Since the delta of the commits was otherwise even _more_ confusing, I've
    left the `CacheControlExtension` present and exported and will remove it in a
    subsequent commit.
    
    Briefly summarizing what the necessary changes were:
    
    1. We no longer use a class instance to house the extension, which was
       necessitated by the `graphql-extensions` API.  This means that uses of
       `this` have been replaced with function scoped variables by the same name.
    2. The logic which actually does the formatting (previously handled by the
       `format` method in `graphql-extension`, now takes place within the plugin
       API's `willSendResponse` method.
    3. *overallCachePolicy*
    4. anything else?

--- packages/apollo-cache-control/src/__tests__/cacheControlSupport.ts
+++ packages/apollo-cache-control/src/__tests__/cacheControlSupport.ts
@@ -1,5 +1,23 @@
-import { buildSchema } from 'graphql';
+import { buildSchema, GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
+import { plugin, CacheControlExtensionOptions, CacheHint } from '../';
+import {
+  GraphQLRequestContext,
+  GraphQLRequestContextExecutionDidStart,
+  GraphQLRequestContextWillSendResponse,
+  GraphQLRequest,
+  GraphQLResponse,
+  ValueOrPromise,
+  ApolloServerPlugin,
+  WithRequired,
+} from 'apollo-server-plugin-base';
+import { InMemoryLRUCache } from "apollo-server-caching";
+import {
+  enablePluginsForSchemaResolvers,
+  symbolRequestListenerDispatcher,
+} from "apollo-server-core/dist/requestPipelineAPI";
+
+import { Dispatcher } from "apollo-server-core/dist/utils/dispatcher";
 
 type FirstArg<F> = F extends (arg: infer A) => any ? A : never;
 
@@ -30,4 +48,117 @@ export function makeExecutableSchemaWithCacheControlSupport(
     ...options,
     typeDefs: augmentTypeDefsWithCacheControlSupport(options.typeDefs),
   });
+}
+
+// This test harness guarantees the presence of `query`.
+type IPluginTestHarnessGraphqlRequest = WithRequired<GraphQLRequest, 'query'>;
+type IPluginTestHarnessExecutionDidStart<TContext> =
+  GraphQLRequestContextExecutionDidStart<TContext> & {
+    request: IPluginTestHarnessGraphqlRequest,
+  };
+
+export async function pluginTestHarness<TContext>({
+  schema,
+  pluginInitializationOptions = Object.create(null),
+  graphqlRequest,
+  overallCachePolicy,
+  executor,
+  context = Object.create(null)
+}: {
+  /**
+   * The schema, which will be mutated, to be received by the executor.
+   */
+  schema?: GraphQLSchema;
+
+  /**
+  * Passed directly to the plugin options.
+  */
+  pluginInitializationOptions?: CacheControlExtensionOptions;
+
+  /**
+   * The `GraphQLRequest` which will be received by the `executor`.  The
+   * `query` is required, and this doesn't support anything more exotic,
+   * like automated persisted queries (APQ).
+   */
+  graphqlRequest: IPluginTestHarnessGraphqlRequest;
+
+  /**
+   * Overall cache control policy.
+   */
+  overallCachePolicy?: Required<CacheHint>;
+
+  /**
+   * This method will be executed to retrieve the response.
+   */
+  executor: (
+    requestContext: IPluginTestHarnessExecutionDidStart<TContext>,
+  ) => ValueOrPromise<GraphQLResponse>;
+
+  /**
+   * (optional) To provide a user context, if necessary.
+   */
+  context?: TContext;
+}): Promise<GraphQLRequestContextWillSendResponse<TContext>> {
+
+  if (schema) {
+    enablePluginsForSchemaResolvers(schema);
+  }
+
+  const pluginInstance: ApolloServerPlugin<TContext> = plugin({
+    ...pluginInitializationOptions,
+  });
+
+  const requestContext: GraphQLRequestContext<TContext> = {
+    logger: console,
+    request: graphqlRequest,
+    metrics: Object.create(null),
+    source: graphqlRequest.query,
+    cache: new InMemoryLRUCache(),
+    context,
+  };
+
+  requestContext.overallCachePolicy = overallCachePolicy;
+
+  if (typeof pluginInstance.requestDidStart !== "function") {
+    throw new Error("Should be impossible as the plugin is defined.");
+  }
+
+  const listener = pluginInstance.requestDidStart(requestContext);
+
+  if (!listener) {
+    throw new Error("Should be impossible to not have a listener.");
+  }
+
+  if (typeof listener.willResolveField !== 'function') {
+    throw new Error("Should be impossible to not have 'willResolveField'.");
+  }
+
+  const dispatcher = new Dispatcher([listener]);
+
+  // Put the dispatcher on the context so `willResolveField` can access it.
+  Object.defineProperty(requestContext.context, symbolRequestListenerDispatcher, {
+    value: dispatcher,
+  });
+
+  const executionDidEnd = dispatcher.invokeDidStartHook(
+    "executionDidStart",
+    requestContext as IPluginTestHarnessExecutionDidStart<TContext>,
+  );
+
+  try {
+    // `response` is readonly, so we'll cast to `any` to assign to it.
+    (requestContext.response as any) = await executor(
+      requestContext as IPluginTestHarnessExecutionDidStart<TContext>,
+    );
+    executionDidEnd();
+  } catch (executionError) {
+    executionDidEnd(executionError);
+  }
+
+  await dispatcher.invokeHookAsync(
+    "willSendResponse",
+    requestContext as GraphQLRequestContextWillSendResponse<TContext>,
+  );
+
+  return requestContext as GraphQLRequestContextWillSendResponse<TContext>;
 }
